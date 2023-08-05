06f0385eae629fa9996011ba4c89f2f5fd4c99a5
Lawrence <lawrencehome21@gmail.com>
2023-08-05 04:57:08 -0700

    second question (js file)

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ const quizData = [
     c: "python",
     d: "Javascript",
     correct: "d",
-  }
+  },
+  {
+    question: "What does CSS stand for",
+    a: "Central style sheets",
+    b: "Cascading style sheets",
+    c: "Cascading simple sheets",
+    d: "Cars style sheets",
+    correct: "b",
+  },
 ]
