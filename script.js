59fb985ae27b200393c61ca49976fa51fb772818
Lawrence <lawrencehome21@gmail.com>
2023-08-08 15:56:28 -0700

    typechecking

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const quizData = [
   },
 ];
 
-const quiz = getElementById('quiz');
+const quiz = document.getElementById('quiz');
 const answerEl = document.querySelectorAll('.answer');
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
@@ -41,7 +41,6 @@ const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
-
 
 let currentQuiz = 0;
 let score = 0;
@@ -65,7 +64,7 @@ function deselectAnswers(){
 }
 
 function getSelected(){
-  let answerEl;
+  let answer;
   answerEl.forEach(answerEl =>{
     if(answerEl.checked){
       answer = answerEl.id;
@@ -85,7 +84,9 @@ submitBtn.addEventListener('click', () =>{
       loadQuiz();
     }
     else{
-      quiz.innerHTML = '<h2>You answered $(score)/$(quizData.length) questions correctly.</h2> <button onclick="location.reload()">Reload</button>'
+      quiz.innerHTML = `
+      <h2>You answered ${score}/${quizData.length} questions correctly.</h2> 
+      <button onclick="location.reload()">Reload</button>`;
     }
   }
 })
