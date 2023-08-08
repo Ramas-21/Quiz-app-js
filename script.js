447ccdf5b4266e4eb45acbd3e845c66e4f456f3c
Lawrence <lawrencehome21@gmail.com>
2023-08-08 03:56:29 -0700

    utilized the DOM elements in javascript

--- script.js
+++ script.js
@@ -33,3 +33,14 @@ const quizData = [
   },
 ];
 
+const quiz = getElementById('quiz');
+const answerEl = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitEl = document.getElementById('submit');
+
+
+
